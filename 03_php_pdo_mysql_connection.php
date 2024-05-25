97971e08d681fa1bfbdeae9af4cfac1bc13a8137
rodolf2 <144448386+rodolf2@users.noreply.github.com>
2024-05-25 12:48:40 +0800

    Add files via upload

---
+++ 03_php_pdo_mysql_connection.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname = 'localhost';
+$db = 'webapp2_activity';
+$user = 'root'; 
+$password = ''; 
+
+$dsn = "mysql:host=$hostname;dbname=$db;charset=UTF8";
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    $pdo = new PDO($dsn, $user, $password, $options);
+
+    if ($pdo) {
+        echo "Connected to the $db database successfully!";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
